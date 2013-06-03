335db25b7362559b4621d97cf7767af338803b94
stephenharnois <stephen.harnois@live.com>
2013-06-03 17:00:50 -0400

    Fininsh user signup

--- Gemfile
+++ Gemfile
@@ -34,5 +34,6 @@ group :test do
   gem 'capybara', '1.1.2'
   gem 'rb-inotify', '~> 0.9'
   gem 'libnotify', '0.5.9'
+  gem 'factory_girl_rails', '4.1.0'
 end
 
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Stephen Harnois"
+    email    "stephen.harnois@live.com"
+    password "password"
+    password_confirmation "password"
+  end
+end
+
